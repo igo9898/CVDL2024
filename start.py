02fb1f24cb8fccfbdf99d84cd2d9e3761c19c300
Simon Zheng <simonw713jb@gmail.com>
2025-01-14 18:07:56 +0800

    3rd commit

---
+++ start.py
@@ -0,0 +1,14 @@
+# %%
+from PyQt5 import QtWidgets
+import controller
+
+
+
+if __name__ == '__main__':
+    import sys
+    app = QtWidgets.QApplication(sys.argv)
+    window = controller.MainWindow_controller()
+    window.show()
+    sys.exit(app.exec_())
+
+
